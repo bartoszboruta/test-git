9ae4d9432b3488014f04fd91db2051a8a6f57ecd
ŁukaszKubok <syrathea@gmail.com>
2019-10-01 16:53:15 +0200

    LK-2 rebased with master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-import Hello from './Hello'
+import logo from "./logo.svg";
+
+import "./App.css";
+
+import Header from "./Header";
+
+import Hello from "./Hello";
 
 function App() {
   return (
     <div className="App">
       <Hello />
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
