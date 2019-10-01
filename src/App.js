1e66385d7e8d2c93d2ad4a940c0448c85d060acb
ŁukaszKubok <syrathea@gmail.com>
2019-10-01 17:06:56 +0200

    One change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import Hello from './Hello'
+import Hello from "./Hello";
 
 function App() {
   return (
     <div className="App">
       <Hello />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header-small">
+        <img src={logo} className="App-logo-big" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
